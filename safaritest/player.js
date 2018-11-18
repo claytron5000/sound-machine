92e5ba11ec572fc336805941d9fdd6b38e43353d
Clayton Salem <claytonjsalem@gmail.com>
2018-11-18 17:56:50 -0500

    Move out of mobile safari testing

---
+++ safaritest/player.js
@@ -0,0 +1,32 @@
+// mobile safari test
+var myAudioContext = new webkitAudioContext();
+
+request = new XMLHttpRequest();
+
+request.open('GET', './audio/train.mp3', true);
+
+request.responseType = 'arraybuffer';
+
+request.addEventListener('load', bufferSound, false);
+
+request.send();
+
+var mySource;
+
+function bufferSound(event) {
+
+    var request = event.target;
+
+    var source = myAudioContext.createBufferSource();
+
+    source.buffer = myAudioContext.createBuffer(request.response, false);
+
+    mySource = source;
+
+    mySource.connect(myAudioContext.destination);
+}
+
+document.querySelector("#playButton").addEventListener("click", function() {
+    mySource.start(0);
+    alert("yeah")
+}, false)

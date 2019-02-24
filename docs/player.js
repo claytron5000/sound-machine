8a1f818f048773ca14dcd9ff2996d441d9b733e2
Clayton Salem <claytonjsalem@gmail.com>
2019-02-24 16:03:14 -0500

    Working pause button, dom handling is hard

--- docs/player.js
+++ docs/player.js
@@ -1,8 +1,16 @@
 class AudioPlayer {
+
     constructor(tracks) {
         const AudioContext = window.AudioContext || window.webkitAudioContext;
         this.audioCtx = new AudioContext();
         this.tracks = tracks;
+        this.mainGainNode = this.audioCtx.createGain();
+        this.handleStart = this.handleStart.bind(this);
+        // this.mainGainNode.connect(this.audioCtx.destination)
+        document.querySelector('#main-volume').addEventListener('input', (e) => {
+            this.mainGainNode.gain.value = e.target.value;
+            console.log(e)
+        }, false);
     }
 
     formatFileName(fileName) {
@@ -14,14 +22,14 @@ class AudioPlayer {
             `<div class="sound-block">
             <h2>${title}</h2>
             <audio type="audio/mpeg"></audio>
-            <button style="cursor: pointer" data-playing="false" role="switch" aria-checked="false" loop="true">play/pause</button>
+            <button style="cursor: pointer" data-playing="false" role="switch" aria-checked="false" loop="true">play</button>
             <span>
                 <label for="volume">volume</label>
                 <input type="range" class="volume" min="0" max="2" value="1" step="0.01" />
             </span>
         </div>`)
     }
-
+    // Safari doesn't return promise, so we return our own.
     promiseDecodeAudioData(arrayBuffer) {
         return new Promise((resolve, reject) => {
             this.audioCtx.decodeAudioData(arrayBuffer, (decodedData) => { resolve(decodedData), (err) => { reject(err) } })
@@ -37,13 +45,53 @@ class AudioPlayer {
             .then(res => res.arrayBuffer())
             .then(buffer => this.promiseDecodeAudioData(buffer))
             .then(decodedAudio => {
-                last.querySelector('button').addEventListener('click', () => {
+                last.querySelector('button').addEventListener('click', (buttonEvent) => {
+
+                    let parent = last.querySelector('button').parentNode;
+                    last.querySelector('button').remove();
+
+                    const newButton = document.createElement('button')
+                    newButton.appendChild(document.createTextNode('pause'));
+
+                    newButton.addEventListener('click', () => {
+                        source.stop()
+                    })
+                    parent.insertAdjacentElement('afterbegin', newButton);
+
                     const source = this.audioCtx.createBufferSource();
                     source.buffer = decodedAudio;
-                    source.connect(this.audioCtx.destination);
+                    const localGain = this.audioCtx.createGain();
+                    last.querySelector('input').addEventListener('input', (sliderEvent) => {
+                        localGain.gain.value = sliderEvent.target.value;
+                    }, false);
+                    // Safari returns undefined, so we have to step-by step connect the nodes instead of chaining.
+                    source.connect(localGain);
+                    localGain.connect(this.mainGainNode);
+                    this.mainGainNode.connect(this.audioCtx.destination);
                     source.start();
+                    source.loop = true;
                 });
+                return decodedAudio;
             })
+    }
+
+    handleStart(buttonEvent) {
+        const source = this.audioCtx.createBufferSource();
+        source.buffer = decodedAudio;
+        const localGain = this.audioCtx.createGain();
+        last.querySelector('input').addEventListener('input', (sliderEvent) => {
+            localGain.gain.value = sliderEvent.target.value;
+        }, false);
+        // Safari returns undefined, so we have to step-by step connect the nodes instead of chaining.
+        source.connect(localGain);
+        localGain.connect(this.mainGainNode);
+        this.mainGainNode.connect(this.audioCtx.destination);
+        source.start();
+        source.loop = true;
+        // buttonEvent.target.removeEventListener('click')
+        buttonEvent.target.addEventListener('click', () => {
+            source.stop();
+        })
     }
 
 }
@@ -51,14 +99,18 @@ class AudioPlayer {
 window.onload = function () {
 
     fetch('./audio/sounds.js')
-        // we could do better and actually response with json string
         .then(response => response.text())
         .then(text => JSON.parse(text).data)
         .then(data => new AudioPlayer(data))
         .then(AudioPlayer => {
-            AudioPlayer.tracks.forEach(track => {
+            AudioPlayer.tracks.forEach((track, index) => {
                 AudioPlayer.writeToDocument(track);
+                // AudioPlayer.tracks[index].decodedAudio = decodedAudio;
             })
         })
         .catch(err => { console.log('Here be errors ' + err) })
 }
+
+
+
+
